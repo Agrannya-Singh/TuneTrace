1778a165a42386580a24c8f4ce687bd3462a2276
Agrannya S <singh.agrannya@gmail.com>
2025-07-09 23:06:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lastfm.freetls.fastly.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

f9a92eaf5fe6056c8af34e14e8bb97ba8543f350
Agrannya S <singh.agrannya@gmail.com>
2025-07-13 04:56:21 +0000

    Error: Invalid src prop (https://lastfm.freetls.fastly.net/i/u/300x300/2

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lastfm.freetls.fastly.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
